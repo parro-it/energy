370a5a608a8d20465b69a8392320bbf01519fdc4
Andrea Parodi <andrea@parro.it>
2015-09-09 16:36:43 +0200

    filtered files with double versions

--- modules/energy-folders-tojson/es6/index.js
+++ modules/energy-folders-tojson/es6/index.js
@@ -9,6 +9,8 @@ import { resolve } from 'path';
 import thenify from 'thenify';
 import _glob from 'glob';
 import { Readable } from 'stream';
+import pairs from 'object-pairs';
+import zip from 'zipmap';
 const glob = thenify(_glob);
 
 const formatChooser = relativeDir => file => {
@@ -26,7 +28,44 @@ const formatChooser = relativeDir => file => {
     .pipe(converter(relative(relativeDir, file.path)));
 };
 
-function countFiles(globs, baseFolder) {
+export function parseFilename(filename) {
+  const parts = filename.split('.');
+  const [, , , sapr, , period, version] = parts[0].split('_');
+  const kindMap = {details: 'details', general: 'recap'};
+  const kind = parts[1] in kindMap ? kindMap[parts[1]] : 'malformed';
+
+  if (kind === 'malformed') {
+    return { kind, filename };
+  }
+
+  return { sapr, period, version: Number(version), kind, filename };
+}
+
+export function pickLastVersion(files) {
+  const grouped = files
+    .filter(f => f.kind !== 'malformed')
+    .reduce((results, f) => {
+      const key = [f.sapr, f.period].join('-');
+      if ( !(key in results) ) {
+        results[key] = {[f.kind]: f};
+      } else if (! (f.kind in results[key])) {
+        results[key][f.kind] = f;
+      } else if (f.version > results[key][f.kind].version) {
+        results[key][f.kind] = f;
+      }
+
+      return results;
+    }, {});
+
+  return zip(pairs(grouped)
+    .filter(([, value]) =>
+      value.details && value.recap &&
+      value.details.version === value.recap.version
+    ));
+}
+
+
+async function countFiles(globs, baseFolder) {
   const absGlob = resolve(baseFolder, globs);
   const opts = {
     cwd: baseFolder,
@@ -35,7 +74,14 @@ function countFiles(globs, baseFolder) {
     nonull: false,
     cwdbase: false
   };
-  return glob(absGlob, opts);
+  const files = await glob(absGlob, opts);
+  const results = pairs(pickLastVersion(
+    files.map(parseFilename)
+  )).reduce((flattened, [, {details, recap}]) => {
+    flattened.push(details.filename, recap.filename);
+    return flattened;
+  }, []);
+  return results;
 }
 
 export default function convertFiles(pattern, relativeDir) {
@@ -65,5 +111,4 @@ export default function convertFiles(pattern, relativeDir) {
 
   return results;
 }
-
 
--- modules/energy-folders-tojson/test/index_test.js
+++ modules/energy-folders-tojson/test/index_test.js
@@ -5,14 +5,15 @@ if (process.env.TEST_RELEASE) {
 import test from 'tape-catch';
 import { resolve } from 'path';
 import concat from 'concat-stream';
-const convertFiles = require(moduleRoot);
+const { parseFilename, pickLastVersion } = require(moduleRoot);
+const convertFiles = require(moduleRoot).default;
 const expected = [
-  {type: 'detail', filename: 'fixtures/file.details.csv', sapr: 'PVI_9988239_001', date: '2015-06-30T22:00:00.000Z', input: 10.2, output: 11.3},
-  {type: 'recap', filename: 'fixtures/file.general.csv', sapr: 'PVI_9988239_001', name: 'uno qualunque', version: 3, date: '2015-07-15T18:27:00.000Z', total: 424242.42}
+  {type: 'detail', filename: 'fixtures/UPNR_PUNTUALE_UPN_1188999_01_201504_2.details.csv', sapr: 'PVI_9988239_001', date: '2015-06-30T22:00:00.000Z', input: 10.2, output: 11.3},
+  {type: 'recap', filename: 'fixtures/UPNR_PUNTUALE_UPN_1188999_01_201504_2.general.csv', sapr: 'PVI_9988239_001', name: 'uno qualunque', version: 3, date: '2015-07-15T18:27:00.000Z', total: 424242.42}
 ];
 
 test('convert details files',  t => {
-  const pattern = resolve(__dirname, './fixtures') + '/**/*.csv';
+  const pattern = resolve(__dirname, './fixtures') + '/*.csv';
   const convertedStream = convertFiles(pattern, __dirname);
   convertedStream.pipe(concat(result => {
     t.deepEqual(result, expected);
@@ -20,9 +21,11 @@ test('convert details files',  t => {
   }));
 });
 
+/*
+
 test('emit files counting events',  t => {
   t.plan(1);
-  const pattern = resolve(__dirname, './fixtures') + '/**/*.csv';
+  const pattern = resolve(__dirname, './fixtures') + '/*.csv';
   const convertedStream = convertFiles(pattern, __dirname);
   const results = [];
   convertedStream.on('filesCounting', filesCounting => {
@@ -39,3 +42,260 @@ test('emit files counting events',  t => {
   });
   convertedStream.on('end', () => {});
 });
+*/
+
+test('convert details new style filenames to objects',  t => {
+  const result = parseFilename('UPNR_PUNTUALE_UPN_1117375_01_201507_1_Nome dell impianto.details.csv');
+  t.deepEqual(result, {
+    filename: 'UPNR_PUNTUALE_UPN_1117375_01_201507_1_Nome dell impianto.details.csv',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  });
+  t.end();
+});
+
+test('convert recap new style filenames to objects',  t => {
+  const result = parseFilename('UPNR_PUNTUALE_UPN_1117375_01_201507_1_Nome dell impianto.general.csv');
+  t.deepEqual(result, {
+    filename: 'UPNR_PUNTUALE_UPN_1117375_01_201507_1_Nome dell impianto.general.csv',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'recap'
+  });
+  t.end();
+});
+
+test('convert details old style filenames to objects',  t => {
+  const result = parseFilename('UPNR_PUNTUALE_UPN_1189246_01_201504_2.details.csv');
+  t.deepEqual(result, {
+    filename: 'UPNR_PUNTUALE_UPN_1189246_01_201504_2.details.csv',
+    sapr: '1189246',
+    period: '201504',
+    version: 2,
+    kind: 'details'
+  });
+  t.end();
+});
+
+test('convert recap old style filenames to objects',  t => {
+  const result = parseFilename('UPNR_PUNTUALE_UPN_1189246_01_201504_2.general.csv');
+  t.deepEqual(result, {
+    filename: 'UPNR_PUNTUALE_UPN_1189246_01_201504_2.general.csv',
+    sapr: '1189246',
+    period: '201504',
+    version: 2,
+    kind: 'recap'
+  });
+  t.end();
+});
+
+test('return null for bad kind',  t => {
+  const result = parseFilename('UPNR_PUNTUALE_UPN_1189246_01_201504_2.bad.csv');
+  t.deepEqual(result, {
+    filename: 'UPNR_PUNTUALE_UPN_1189246_01_201504_2.bad.csv',
+    kind: 'malformed'
+  });
+  t.end();
+});
+
+test('return malformed kind for malformed filenames',  t => {
+  const result = parseFilename('Curve_6467 (10)');
+  t.deepEqual(result, {
+    filename: 'Curve_6467 (10)',
+    kind: 'malformed'
+  });
+  t.end();
+});
+
+test('group files by sapr & period',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201506',
+    version: 1,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1189246',
+    period: '201504',
+    version: 2,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1189246',
+    period: '201503',
+    version: 2,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'recap'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201506',
+    version: 1,
+    kind: 'recap'
+  }, {
+    filename: 'file.txt',
+    sapr: '1189246',
+    period: '201504',
+    version: 2,
+    kind: 'recap'
+  }, {
+    filename: 'file.txt',
+    sapr: '1189246',
+    period: '201503',
+    version: 2,
+    kind: 'recap'
+  }];
+
+  const result = pickLastVersion(files);
+  t.deepEqual(Object.keys(result), [
+    '1117375-201507', '1117375-201506',
+    '1189246-201504', '1189246-201503'
+  ]);
+  t.end();
+});
+
+test('skip malformed files',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'recap'
+  }, {
+    filename: 'file.txt',
+    kind: 'malformed'
+  }];
+
+  const result = pickLastVersion(files);
+  t.deepEqual(Object.keys(result), [
+    '1117375-201507'
+  ]);
+  t.end();
+});
+
+
+test('result values are grouped in details & recap',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'recap'
+  }];
+
+  const result = pickLastVersion(files);
+  t.equal(Object.keys(result).length, 1);
+  t.same(result['1117375-201507'].details, files[0]);
+  t.same(result['1117375-201507'].recap, files[1]);
+  t.end();
+});
+
+
+test('skip obsolete file versions',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 3,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 3,
+    kind: 'recap'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 2,
+    kind: 'details'
+  }];
+
+  const result = pickLastVersion(files);
+  t.equal(Object.keys(result).length, 1);
+  t.same(result['1117375-201507'].details, files[1]);
+  t.end();
+});
+
+test('skip file with unmatching versions between details and recap',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  }, {
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 3,
+    kind: 'recap'
+  }];
+
+  const result = pickLastVersion(files);
+  t.equal(Object.keys(result).length, 0);
+  t.end();
+});
+
+test('skip files with only details',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'details'
+  }];
+
+  const result = pickLastVersion(files);
+  t.equal(Object.keys(result).length, 0);
+  t.end();
+});
+
+test('skip files with only recap',  t => {
+  const files = [{
+    filename: 'file.txt',
+    sapr: '1117375',
+    period: '201507',
+    version: 1,
+    kind: 'recap'
+  }];
+
+  const result = pickLastVersion(files);
+  t.equal(Object.keys(result).length, 0);
+  t.end();
+});
+
